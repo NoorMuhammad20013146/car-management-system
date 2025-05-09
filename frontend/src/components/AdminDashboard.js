09026393d6d37dd369203f2ccb1d24e3cc73fd5a
NoorMuhammad20013146 <20013146@mydbs.ie>
2025-05-10 00:36:46 +0500

    Code refactoring for Auth and Admin Pages

--- frontend/src/components/AdminDashboard.js
+++ frontend/src/components/AdminDashboard.js
@@ -1,4 +1,3 @@
-// src/components/AdminDashboard.js
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
